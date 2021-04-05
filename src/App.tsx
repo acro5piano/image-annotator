4d10cc83b9802a232a13d7dc672a3c7600341c08
gosho-kazuya <ketsume0211@gmail.com>
2021-04-05 15:05:18 +0900

    fix png

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ export function App() {
 
   return (
     <div>
-      <nav className="px-8 py-2 bg-indigo-600 flex justify-between items-center fixed top-0 w-full text-xl">
-        <div className="text-white">Image Annotator</div>
+      <nav className="px-8 py-2 bg-gray-900 flex justify-between items-center fixed top-0 w-full text-xl">
+        <div className="text-white flex items-center">Image Annotator</div>
         <div className="flex items-center">
           <div
             className="text-white cursor-pointe flex items-center text-white hover:bg-indigo-700 hover:text-white px-2 py-1 cursor-pointer"
